89543d2258b07faf48d8eac2b38090f24b00d6da
Mirinae8 <gdjw816@naver.com>
2023-09-18 13:58:34 +0900

    태그의 class를 리액트에 맞게 className으로 변경

--- src/main/index.js
+++ src/main/index.js
@@ -8,6 +8,7 @@ function MainPage() {
   // state 사용위해 products는 배열이라 파라미터로 빈배열을 넣음
   const [products, setProducts] = React.useState([]);
   //   state에서 then에 들어간 setProducts가 호출 될 때 마다 코드가 다시 시작 즉 데이터 통신부터 랜덩링까지 지속적으로 반복된다
+  //  한 번만 호출하도록 useEffect 사용
   React.useEffect(function () {
     axios
       .get(
@@ -40,19 +41,20 @@ function MainPage() {
           {/* 기존에 innerHTML로 했던 걸 리액트에서는 jsx 문법으로*/}
           {products.map(function (product, index) {
             return (
-              <div class="product-card">
+              // 리액트에서는 태그안에 class 대신 className을 넣는다 (jsx 문법 관련?)
+              <div className="product-card">
                 {/* <Link className="product-link" to={"/products/" + index}> */}
                 {/* ES6 문법 Template Literal 활용시 */}
                 <Link className="product-link" to={`/products/${index}`}>
                   <div>
-                    <img class="product-image" src={product.imageUrl} />
+                    <img className="product-image" src={product.imageUrl} />
                   </div>
-                  <div class="product-contents">
-                    <span class="product-name">{product.name}</span>
-                    <span class="product-price">{product.price}원</span>
-                    <div class="product-seller">
+                  <div className="product-contents">
+                    <span className="product-name">{product.name}</span>
+                    <span className="product-price">{product.price}원</span>
+                    <div className="product-seller">
                       <img
-                        class="product-avatar"
+                        className="product-avatar"
                         src="images/icons/avatar.png"
                       />
                       <span>{product.seller}</span>
