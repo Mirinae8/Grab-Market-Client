b3bd6e97403f5115afdbbf2fc8aae2a9c20db221
Mirinae8 <gdjw816@naver.com>
2023-09-25 17:23:46 +0900

    fly.io 클라우드와 통신하도록 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,8 @@
 // 상수 선언용 파일
-export const API_URL = "http://localhost:8080";
+// 실제 돌아가고 있는 서버로 변경 (fly.io에 올린 서버)
+// 삼항연산자로 실제 서버가 돌아가지 않으면 로컬과 연결하도록
+// process.env.NODE_ENV 는 내부에서 돌릴때는 "develpoment가 들어감" 클라우드면 "production"
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://grab-market-server-gdjw.fly.dev"
+    : "http://localhost:8080";
